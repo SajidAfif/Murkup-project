41daee82ea24c780ab16047839e0492455da4707
Molla MD Sabit <msabit212167@bscse.uiu.ac.bd>
2026-05-23 19:22:08 +0600

    Murkup Project

---
+++ frontend/hello.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int sub(int a, int b) {
+    return a - b;
+}
+
+int mul(int a, int b) {
+    return a * b;
+}
+
+int div(int a, int b) {
+    return a / b;
+}
+
+
+int main() {
+    int a = 10, b = 5;
+    printf("Addition: %d\n", add(a, b));
+    printf("Subtraction: %d\n", sub(a, b));
+    printf("Multiplication: %d\n", mul(a, b));
+    printf("Division: %d\n", div(a, b));
+    return 0;
+}
